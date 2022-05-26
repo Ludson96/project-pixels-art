e2ce447f2e11d5297ece1f84b4cf91e0a782add1
Ludson <ludson_ps27@hotmail.com>
2022-05-26 10:57:00 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let red = document.getElementById('red');
 let blue = document.getElementById('blue');
 let green = document.getElementById('green');
 let pixel = document.getElementsByClassName('pixel');
+let limpar = document.getElementById('clear-board');
 
 function selectColor(event) {
   const elementSelected = document.querySelector('.selected');
@@ -15,7 +16,7 @@ red.addEventListener('click', selectColor);
 blue.addEventListener('click', selectColor);
 green.addEventListener('click', selectColor);
 
-function paintColor (event) {
+function paintColor(event) {
   let elementSelected = document.getElementsByClassName('selected')[0];
   const cssSelected = getComputedStyle(elementSelected, null);
 
@@ -23,6 +24,14 @@ function paintColor (event) {
   event.target.style.backgroundColor = bgColor;
 }
 
-  for (let index = 0; index < pixel.length; index += 1){
-    pixel[index].addEventListener('click', paintColor)
+for (let index = 0; index < pixel.length; index += 1) {
+  pixel[index].addEventListener('click', paintColor)
+}
+
+function clear() {
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].style.backgroundColor = 'white';
   }
+}
+
+limpar.addEventListener('click', clear);
