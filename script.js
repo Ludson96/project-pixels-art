0df5021f59a18419b5aaaa9f022c003b0d29adcd
Ludson <ludson_ps27@hotmail.com>
2022-06-04 12:48:36 -0300

    lint

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let black = document.getElementById('black');
-let red = document.getElementById('red');
-let blue = document.getElementById('blue');
-let green = document.getElementById('green');
-// let pixel = document.getElementsByClassName('pixel');
-let limpar = document.getElementById('clear-board');
+const black = document.getElementById('black');
+const red = document.getElementById('red');
+const blue = document.getElementById('blue');
+const green = document.getElementById('green');
+let pixel = document.getElementsByClassName('pixel');
+const limpar = document.getElementById('clear-board');
 
 function selectColor(event) {
   const elementSelected = document.querySelector('.selected');
@@ -16,22 +16,22 @@ red.addEventListener('click', selectColor);
 blue.addEventListener('click', selectColor);
 green.addEventListener('click', selectColor);
 
-//CRIANDO MEU BOARD
+  //CRIANDO MEU BOARD
 function createBoard() {
   for (let linha = 0; linha < 5; linha += 1) {
-    let newLi = document.createElement('li');
-    let getPixelBoard = document.getElementById('pixel-board');
+    const newLi = document.createElement('li');
+    const getPixelBoard = document.getElementById('pixel-board');
     getPixelBoard.appendChild(newLi);
     for (let coluna = 0; coluna < 5; coluna += 1) {
-      let newDiv = document.createElement('div');
-      newDiv.className = 'pixel'
-      newLi.appendChild(newDiv)
+      const newDiv = document.createElement('div');
+      newDiv.className = 'pixel';
+      newLi.appendChild(newDiv);
     }
   }
 }
-createBoard()
+createBoard();
 
-//Função evento de limpar 
+  //Função evento de limpar
 function clear() {
   for (let index = 0; index < pixel.length; index += 1) {
     pixel[index].style.backgroundColor = 'white';
@@ -40,49 +40,38 @@ function clear() {
 
 limpar.addEventListener('click', clear);
 
-// //Requisito 10 
+// //Requisito 10
 // RESTRIÇÃO DO INPUT
 function restricaoInput() {
-  let newInput = document.createElement('input');
+  const newInput = document.createElement('input');
   newInput.setAttribute('type', 'number');
   newInput.setAttribute('value', 'default');
   newInput.setAttribute('max', '50');
   newInput.setAttribute('min', '1');
   newInput.id = 'board-size';
-  let board = document.getElementById('board');
+  const board = document.getElementById('board');
   board.appendChild(newInput);
 }
 restricaoInput();
 
 // botão VQV
-function vqv() {
-  let vqv = document.createElement('input');
+function btnVQV() {
+  const vqv = document.createElement('input');
   vqv.id = 'generate-board';
   vqv.setAttribute('type', 'submit');
   vqv.setAttribute('value', 'VQV');
-  vqv.innerText = 'VQV'
+  vqv.innerText = 'VQV';
+  const board = document.getElementById('board');
   board.appendChild(vqv);
 }
-vqv();
+btnVQV();
 
-//APAGANDO PIXELS
+  //APAGANDO PIXELS
 function redPixels() {
   const getLi = document.querySelectorAll('li');
   for (let index = 0; index < getLi.length; index += 1) {
-    getLi[index].remove()
+    getLi[index].remove();
   }
-}
-
-//VERIFICANDO TAMANHO DO MEU BOARD
-function verificaTamanho (n) {
-  if (n == '') {
-    alert('Board inválido!')
-  } else if(n < 5) {
-    n = 5;
-  } else if (n > 50) {
-    n = 50;
-  }
-  createPixels(n);
 }
 
 //CRIANDO PIXELS COM O VALOR DO INPUT
@@ -99,10 +88,22 @@ function createPixels(n) {
   }
 }
 
-// getQuantidade.addEventListener('input', createPixels)
+  //VERIFICANDO TAMANHO DO MEU BOARD
+function verificaTamanho(n) {
+  if (n == '') {
+    alert('Board inválido!');
+  } else if (n < 5) {
+    n = 5;
+  } else if (n > 50) {
+    n = 50;
+  }
+  createPixels(n);
+}
 
-// //Requisito 12 - gerar cor aleatória
-// https://www.horadecodar.com.br/2022/01/16/gerar-cor-aleatoria-com-javascript/
+
+
+  // //Requisito 12 - gerar cor aleatória
+  // https://www.horadecodar.com.br/2022/01/16/gerar-cor-aleatoria-com-javascript/
 function generateColor() {
 
   const letters = '0123456789ABCDEF';
@@ -123,13 +124,11 @@ green.style.backgroundColor = generateColor();
 //PINTANDO OS QUADRADOS
 function paintColor(event) {
   let elementSelected = document.getElementsByClassName('selected')[0];
-  // let bgColor = elementSelected[0].style.backgroundColor;
   const cssSelected = getComputedStyle(elementSelected, null);
 
   let bgColor = cssSelected.getPropertyValue('background-color');
   event.target.style.backgroundColor = bgColor;
 }
-let pixel = document.getElementsByClassName('pixel');
 for (let index = 0; index < pixel.length; index += 1) {
   pixel[index].addEventListener('click', paintColor)
 }
